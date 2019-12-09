3c4fc7d8463b068d666e519c1c2ebfe42457b4a8
kunal patil <kunal.m.patil@gmail.com>
2019-12-09 19:27:57 +0530

    Enable Jaeger OpenTracing

--- src/main/java/com/example/demo/BarApplication.java
+++ src/main/java/com/example/demo/BarApplication.java
@@ -2,6 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.jaegertracing.Configuration;
+import io.jaegertracing.internal.JaegerTracer;
 
 @SpringBootApplication
 public class BarApplication {
@@ -9,5 +13,43 @@ public class BarApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BarApplication.class, args);
 	}
-
+	
+	@Bean
+    public static JaegerTracer getTracer() {
+        
+        Configuration.Propagation propogateConfig =  Configuration.Propagation.B3;
+        Configuration.CodecConfiguration codecConfig =  Configuration
+        		.CodecConfiguration
+        		.fromEnv()
+        		.withPropagation(propogateConfig) ;
+        Configuration config = Configuration.fromEnv();	
+        config.withCodec(codecConfig);
+        return config.getTracer();
+	}
+	
+	/*@Bean
+    public static JaegerTracer getTracer() {
+        Configuration.SamplerConfiguration samplerConfig = Configuration
+        		.SamplerConfiguration
+        		.fromEnv()
+        		.withType("const")
+        		.withParam(1);
+        Configuration.ReporterConfiguration reporterConfig = Configuration
+        		.ReporterConfiguration
+        		.fromEnv()
+        		.withLogSpans(true);
+        Configuration.Propagation propogateConfig =  Configuration.Propagation.B3;
+        Configuration.CodecConfiguration codecConfig =  Configuration
+        		.CodecConfiguration
+        		.fromEnv()
+        		.withPropagation(propogateConfig);
+        
+        Configuration config = new Configuration("Bar Service")
+        		.withSampler(samplerConfig)
+        		.withReporter(reporterConfig)
+        		.withCodec(codecConfig);
+        
+        return config.getTracer();
+	}*/
+	
 }
--- src/main/java/com/example/demo/controller/BarController.java
+++ src/main/java/com/example/demo/controller/BarController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.data.Movie;
@@ -16,8 +18,9 @@ public class BarController {
 	private MovieService movieService;
 	
 	@GetMapping("/data")
-	public List<Movie> getAll() {
+	public List<Movie> getAll(@RequestHeader HttpHeaders headers) {
 		System.out.println("/data request in bar");
+		System.out.println(headers);
 		return movieService.getAllMovies();
 	}
 }
